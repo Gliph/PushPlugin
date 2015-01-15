bc5e57587a40dc2a0271b0f4fd9e3e7ab2668109
Nicholas Asch <github@pilotsworld.net>
2015-01-15 11:21:45 -0800

    customizing for Gliph

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -9,6 +9,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -17,137 +18,132 @@ import com.google.android.gcm.GCMBaseIntentService;
 
 @SuppressLint("NewApi")
 public class GCMIntentService extends GCMBaseIntentService {
+    public static final int NOTIFICATION_ID = 237;
+    private static final String TAG = "GCMIntentService";
+    
+    public GCMIntentService() {
+        super("GCMIntentService");
+    }
 
-	private static final String TAG = "GCMIntentService";
-	
-	public GCMIntentService() {
-		super("GCMIntentService");
-	}
+    @Override
+    public void onRegistered(Context context, String regId) {
 
-	@Override
-	public void onRegistered(Context context, String regId) {
+        Log.v(TAG, "onRegistered: "+ regId);
 
-		Log.v(TAG, "onRegistered: "+ regId);
+        JSONObject json;
 
-		JSONObject json;
+        try {
+            json = new JSONObject().put("event", "registered");
+            json.put("regid", regId);
 
-		try
-		{
-			json = new JSONObject().put("event", "registered");
-			json.put("regid", regId);
+            Log.v(TAG, "onRegistered: " + json.toString());
 
-			Log.v(TAG, "onRegistered: " + json.toString());
+            // Send this JSON data to the JavaScript application above EVENT should be set to the msg type
+            // In this case this is the registration ID
+            PushPlugin.sendJavascript( json );
 
-			// Send this JSON data to the JavaScript application above EVENT should be set to the msg type
-			// In this case this is the registration ID
-			PushPlugin.sendJavascript( json );
+        } catch(JSONException e) {
+            // No message to the user is sent, JSON failed
+            Log.e(TAG, "onRegistered: JSON exception");
+        }
+    }
 
-		}
-		catch( JSONException e)
-		{
-			// No message to the user is sent, JSON failed
-			Log.e(TAG, "onRegistered: JSON exception");
-		}
-	}
+    @Override
+    public void onUnregistered(Context context, String regId) {
+        Log.d(TAG, "onUnregistered - regId: " + regId);
+    }
 
-	@Override
-	public void onUnregistered(Context context, String regId) {
-		Log.d(TAG, "onUnregistered - regId: " + regId);
-	}
+    @Override
+    protected void onMessage(Context context, Intent intent) {
+        Log.d(TAG, "onMessage - context: " + context);
 
-	@Override
-	protected void onMessage(Context context, Intent intent) {
-		Log.d(TAG, "onMessage - context: " + context);
-
-		// Extract the payload from the message
-		Bundle extras = intent.getExtras();
-		if (extras != null)
-		{
-			// if we are in the foreground, just surface the payload, else post it to the statusbar
+        // Extract the payload from the message
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            // if we are in the foreground, just surface the payload, else post it to the statusbar
             if (PushPlugin.isInForeground()) {
-				extras.putBoolean("foreground", true);
+                extras.putBoolean("foreground", true);
                 PushPlugin.sendExtras(extras);
-			}
-			else {
-				extras.putBoolean("foreground", false);
+            } else {
+                extras.putBoolean("foreground", false);
 
                 // Send a notification if there is a message
-                if (extras.getString("message") != null && extras.getString("message").length() != 0) {
+                if (extras.getString("msg") != null && extras.getString("msg").length() != 0) {
                     createNotification(context, extras);
                 }
             }
         }
-	}
+    }
 
-	public void createNotification(Context context, Bundle extras)
-	{
-		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		String appName = getAppName(this);
+    private int getDefaults(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("notifications", Context.MODE_PRIVATE);
+        int defaults = 0;
+        if (prefs.getBoolean("lights", true)) {
+            defaults = defaults | Notification.DEFAULT_LIGHTS;
+        }
+        if (prefs.getBoolean("sound", true)) {
+            defaults = defaults | Notification.DEFAULT_SOUND;
+        }
+        if (prefs.getBoolean("vibrate", true)) {
+            defaults = defaults | Notification.DEFAULT_VIBRATE;
+        }
+        return defaults;
+    }
 
-		Intent notificationIntent = new Intent(this, PushHandlerActivity.class);
-		notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		notificationIntent.putExtra("pushBundle", extras);
+    public void createNotification(Context context, Bundle extras) {
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String appName = getAppName(this);
 
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-		
-		int defaults = Notification.DEFAULT_ALL;
+        Intent notificationIntent = new Intent(this, PushHandlerActivity.class);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        notificationIntent.putExtra("pushBundle", extras);
 
-		if (extras.getString("defaults") != null) {
-			try {
-				defaults = Integer.parseInt(extras.getString("defaults"));
-			} catch (NumberFormatException e) {}
-		}
-		
-		NotificationCompat.Builder mBuilder =
-			new NotificationCompat.Builder(context)
-				.setDefaults(defaults)
-				.setSmallIcon(context.getApplicationInfo().icon)
-				.setWhen(System.currentTimeMillis())
-				.setContentTitle(extras.getString("title"))
-				.setTicker(extras.getString("title"))
-				.setContentIntent(contentIntent)
-				.setAutoCancel(true);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        
+        int defaults = getDefaults(context);
 
-		String message = extras.getString("message");
-		if (message != null) {
-			mBuilder.setContentText(message);
-		} else {
-			mBuilder.setContentText("<missing message content>");
-		}
+        String sound = extras.getString("sound");
+        if (sound == null) {
+            defaults = 0;
+        }
+        
+        NotificationCompat.Builder mBuilder =
+            new NotificationCompat.Builder(context)
+                .setDefaults(defaults)
+                .setSmallIcon(context.getApplicationInfo().icon)
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle("Gliph Notification")
+                .setTicker("Gliph Notification")
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true);
 
-		String msgcnt = extras.getString("msgcnt");
-		if (msgcnt != null) {
-			mBuilder.setNumber(Integer.parseInt(msgcnt));
-		}
-		
-		int notId = 0;
-		
-		try {
-			notId = Integer.parseInt(extras.getString("notId"));
-		}
-		catch(NumberFormatException e) {
-			Log.e(TAG, "Number format exception - Error parsing Notification ID: " + e.getMessage());
-		}
-		catch(Exception e) {
-			Log.e(TAG, "Number format exception - Error parsing Notification ID" + e.getMessage());
-		}
-		
-		mNotificationManager.notify((String) appName, notId, mBuilder.build());
-	}
-	
-	private static String getAppName(Context context)
-	{
-		CharSequence appName = 
-				context
-					.getPackageManager()
-					.getApplicationLabel(context.getApplicationInfo());
-		
-		return (String)appName;
-	}
-	
-	@Override
-	public void onError(Context context, String errorId) {
-		Log.e(TAG, "onError - errorId: " + errorId);
-	}
+        String message = extras.getString("msg");
+        if (message != null) {
+            mBuilder.setContentText(message);
+        } else {
+            mBuilder.setContentText("<missing message content>");
+        }
 
+        String msgcnt = extras.getString("badge");
+        if (msgcnt != null) {
+            mBuilder.setNumber(Integer.parseInt(msgcnt));
+        }
+        
+        mNotificationManager.notify((String) appName, NOTIFICATION_ID, mBuilder.build());
+    }
+
+    private static String getAppName(Context context)
+    {
+        CharSequence appName = 
+            context
+                .getPackageManager()
+                .getApplicationLabel(context.getApplicationInfo());
+        
+        return (String) appName;
+    }
+    
+    @Override
+    public void onError(Context context, String errorId) {
+        Log.e(TAG, "onError - errorId: " + errorId);
+    }
 }
